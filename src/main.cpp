1373c5988d983a22b48f56c1ddad09e627b504f3
nicung <nicung@student.ubc.ca>
2025-12-02 23:31:17 -0800

    blink at you on serial input

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,57 @@
 #include <Arduino.h>
+#include <stdio.h>
 
 // put function declarations here:
-int myFunction(int, int);
+void flashRGB(int ms);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // int result = myFunction(2, 3);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // loop() is called repeatedly by Arduino framework
+  if (Serial.available() > 0) {
+    (void)Serial.read(); // consume the incoming byte
+    flashRGB(100);
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void flashRGB(int ms) {
+  digitalWrite(6, HIGH); // R
+  digitalWrite(7, HIGH); // G
+  digitalWrite(8, HIGH); // B
+  delay(ms);
+  digitalWrite(6, HIGH); // R
+  digitalWrite(7, LOW);  // G
+  digitalWrite(8, HIGH); // B
+  delay(ms);
+  digitalWrite(6, HIGH); // R
+  digitalWrite(7, HIGH); // G
+  digitalWrite(8, LOW);  // B
+  delay(ms);
+  digitalWrite(6, LOW);  // R
+  digitalWrite(7, HIGH); // G
+  digitalWrite(8, HIGH); // B
+  delay(ms);
+  digitalWrite(6, LOW);  // R
+  digitalWrite(7, LOW);  // G
+  digitalWrite(8, HIGH); // B
+  delay(ms);
+  digitalWrite(6, LOW);  // R
+  digitalWrite(7, HIGH); // G
+  digitalWrite(8, LOW);  // B
+  delay(ms);
+  digitalWrite(6, HIGH); // R
+  digitalWrite(7, LOW);  // G
+  digitalWrite(8, LOW);  // B
+  delay(ms);
+  digitalWrite(6, LOW); // R
+  digitalWrite(7, LOW);  // G
+  digitalWrite(8, LOW);  // B
 }
